2584d91a3b5f196e0d0760bc1c302155c6afdcb3
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-13 11:51:14 +0100

    feat: initialize React project with Vite, add basic components and styles
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling with a heading class.
    - Implemented App component to render Post components.
    - Added Heading component for future use.
    - Included React logo SVG in assets.
    - Developed Post component to display author and body.
    - Created Post.module.css for Post component styling.
    - Established global styles in index.css.
    - Set up main entry point in main.jsx for rendering the App.
    - Configured Vite with a basic vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <Post author="Mazillian" body="React.js is awesome" />
+      <Post author="Manuel" body="Check out the full content" />
+    </>
+  );
+}
+
+export default App;
