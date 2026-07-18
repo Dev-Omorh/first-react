78eb8b606f8e6ab8920623c1938cdf5bd2953bcf
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-18 01:14:58 +0100

    feat: update NewPost and PostsList components to handle body and author input changes

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Post from "./Post";
 import classes from "./Post.module.css";
 import NewPost from "./NewPost";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [author, setEnterBody] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.post}>
-        <Post author="Silian" body="React.js is awesome" />
-        <Post author="Kanuel" body="Check out the full content" />
+        <Post author="Silian" body={enteredBody} />
+        <Post author="Kanuel" body={authorCh} />
       </ul>
     </>
   );
