52db8c66263efe49d22a41b0a52902a5d7e15bab
omoladeajala <shadowcoderx.02@gmail.com>
2026-07-20 07:34:07 +0100

    feat: implement Modal component and integrate it into PostsList; update NewPost styling

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Post from "./Post";
 import classes from "./Post.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,10 +22,14 @@ function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
       <div className={classes.post}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Maxilian" body="Check out the full course" />
