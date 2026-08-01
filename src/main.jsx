28fd78ab6c188e80324d1e6aad1172342baab566
omoladeajala <shadowcoderx.02@gmail.com>
2026-08-01 19:08:24 +0100

    feat: implement RootLayout component and update routing structure in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import NewPost from "./components/NewPost";
+import RootLayout from "./routes/RootLayout.jsx";
+import "./index.css";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
