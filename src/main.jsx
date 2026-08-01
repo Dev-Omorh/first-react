63baf2e7819cc67f19d7159ac2d781a704c2aff9
omoladeajala <shadowcoderx.02@gmail.com>
2026-08-01 18:51:10 +0100

    feat: add react-router-dom for routing and implement basic routing structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <NewPost /> },
+]);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
