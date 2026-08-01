364019937dd16ca8ce40a5a638648ab30a9deae3
omoladeajala <shadowcoderx.02@gmail.com>
2026-08-01 02:08:16 +0100

    feat: add loading state and conditional rendering for posts in PostsList component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,15 @@ import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("https//localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -36,7 +39,7 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      {posts.length > 0 && (
+      {isFetching && posts.length > 0 && (
         <ul className={classes.post}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
@@ -44,10 +47,16 @@ function PostsList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          {" "}
+          <p>Loading posting....</p>{" "}
         </div>
       )}
     </>
